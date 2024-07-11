6e7f33efd7210587028fa1ac04b5df7f438f5fca
DeepakSR-12 <deepakdeepakeng@gmail.com>
2024-07-11 14:19:07 +0530

    remove unwanted code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,24 @@
-'use client';
+"use client";
 
-import './globals.scss';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from '@/contexts/AuthContext';
-import { ThemeProvider } from '@/contexts/ThemeContext';
-import { GlobalContextProvider } from '@/contexts/AppContext';
-import ReactQueryPvorider from '@/contexts/ReactQueryPvorider';
-import { SessionProvider } from 'next-auth/react';
-import { Header } from '@/components/header';
-import { Footer } from '@/components/footer';
-
-const queryClient = new QueryClient();
-
+import "./globals.scss";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/footer";
+import { ThemeProvider } from "@/contexts/ThemeContext";
 interface RootLayoutProps {
   children: React.ReactNode;
 }
-
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
-        <ReactQueryPvorider>
-          <SessionProvider>
-            <ThemeProvider>
-              <AuthProvider>
-                <GlobalContextProvider>
-                  <div className="layout">
-                    <Header />
-                    <main className="main">
-                      {children}
-                    </main>
-                    <Footer />
-                  </div>
-                </GlobalContextProvider>
-              </AuthProvider>
-            </ThemeProvider>
-          </SessionProvider>
-        </ReactQueryPvorider>
+        <ThemeProvider>
+          <div className="layout">
+            <Header />
+            <main className="main">{children}</main>
+            <Footer />
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,42 +1,15 @@
-import React from 'react';
-import styles from './Header.module.scss';
-import Avatar from '@/components/ui/Avatar'
-import { useGlobalContext } from '@/contexts/AppContext'
-import { useRouter } from 'next/navigation';
-import { ThemeProvider, useTheme } from '@/contexts/ThemeContext';
-
-const ThemeToggleButton: React.FC = () => {
-  const { toggleTheme } = useTheme();
-  return <button onClick={toggleTheme}>Toggle Theme</button>;
-};
-
+import React from "react";
+import styles from "./Header.module.scss";
+import { useRouter } from "next/navigation";
 
 export const Header: React.FC = () => {
   const router = useRouter();
-  const { state } = useGlobalContext();
   const navigateToHome = () => {
-    // if(state?.onboarded) {
-    //   router.push('/dashboard')
-    // } else {
-    //   router.push('/onboarding')
-    // }
-    router.push('/dashboard')
-  }
+    router.push("/dashboard");
+  };
   return (
     <header className={styles.header}>
       <h1 onClick={navigateToHome}>Aloha Earn</h1>
-      {state.user && (
-        <div className={styles.profile}>
-          <Avatar
-            url={state.user.image}
-            name={state.user.name}
-            onClick={() => router.push('/profile')}
-          />
-          {/* <p className={styles.name}>{state.user.name}</p> */}
-        </div>
-      )}
-      {/* <ThemeToggleButton /> */}
-      {/* Add navigation links here */}
     </header>
   );
 };
