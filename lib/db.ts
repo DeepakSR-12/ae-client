064de905c9679120002fa2626094e10dfc35fb4c
DeepakSR-12 <deepakdeepakeng@gmail.com>
2024-07-23 17:35:22 +0530

    add db cache logic

--- lib/db.ts
+++ lib/db.ts
@@ -6,6 +6,9 @@ if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
 
+// Cache only in local environment
+const isLocal = process.env.NODE_ENV === "development";
+
 let cached = (global as any).mongoose;
 
 if (!cached) {
@@ -13,14 +16,18 @@ if (!cached) {
 }
 
 async function dbConnect() {
-  if (cached.conn) {
+  if (isLocal && cached.conn) {
     return cached.conn;
   }
 
-  if (!cached.promise) {
+  if (isLocal && !cached.promise) {
     cached.promise = mongoose.connect(MONGODB_URI!).then((mongoose) => {
       return mongoose;
     });
+  } else if (!isLocal) {
+    // In production, create a new connection instance
+    cached.conn = await mongoose.connect(MONGODB_URI!);
+    return cached.conn;
   }
   cached.conn = await cached.promise;
   return cached.conn;
